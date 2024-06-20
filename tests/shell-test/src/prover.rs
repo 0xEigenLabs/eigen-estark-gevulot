823ba2f38038fa4f779dd1fc5ac83c621889784c
Ubuntu <devadmin@zkevm-prover-uat-vm1.qf0bt0i4yvdutbhtrpicnmptkg.ix.internal.cloudapp.net>
2024-06-20 14:36:26 +0000

    fix clippy

--- tests/shell-test/src/prover.rs
+++ tests/shell-test/src/prover.rs
@@ -1,14 +1,10 @@
 extern crate clap;
 use clap::{command, Parser};
 
-
 use starky::prove::stark_prove;
-use std::time::Instant;
-use std::fs;
 
 use std::fs::File;
 use std::io::Write;
-
 
 #[derive(Debug, Parser, Default)]
 #[command(about, version, no_binary_name(true))]
@@ -43,48 +39,60 @@ use gevulot_shim::{Task, TaskResult};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-fn main()-> Result<()>  {
-   gevulot_shim::run(run_task)
+fn main() -> Result<()> {
+    gevulot_shim::run(run_task)
 }
 
 fn run_task(task: Task) -> Result<TaskResult> {
-
     env_logger::init();
- 
-    println!("0xEigenLabs prover : task.args: {:?}", &task.args);
-    
-    let args =  Cli::parse_from(&task.args);
 
-    log::info!("parameters: proof file:{}; circom fiel:{}",args.zkin,args.circom_file);
-    log::info!("parameters: args.stark_struct:{} ; args.piljson:{}; args.const_pols:{}", args.stark_struct, args.piljson, args.const_pols);
-    log::info!("parameters: norm_stage:{} ; args.skip_main:{} ; args.agg_stage:{}", args.norm_stage, args.skip_main, args.agg_stage);
+    println!("0xEigenLabs prover : task.args: {:?}", &task.args);
+
+    let args = Cli::parse_from(&task.args);
+
+    log::info!(
+        "parameters: proof file:{}; circom fiel:{}",
+        args.zkin,
+        args.circom_file
+    );
+    log::info!(
+        "parameters: args.stark_struct:{} ; args.piljson:{}; args.const_pols:{}",
+        args.stark_struct,
+        args.piljson,
+        args.const_pols
+    );
+    log::info!(
+        "parameters: norm_stage:{} ; args.skip_main:{} ; args.agg_stage:{}",
+        args.norm_stage,
+        args.skip_main,
+        args.agg_stage
+    );
 
     let mut log_file = File::create("/workspace/test.log")?;
-    write!(log_file, "stark_struct:{}\n",  &args.stark_struct)?;
-    write!(log_file, "piljson:{}\n",  &args.piljson)?;
-    write!(log_file, "const_pols:{}\n",  &args.const_pols)?;
-    write!(log_file, "cm_pols:{}\n",  &args.cm_pols)?;
-    write!(log_file, "proof:{}\n",  &args.zkin)?;
+    write!(log_file, "stark_struct:{}\n", &args.stark_struct)?;
+    write!(log_file, "piljson:{}\n", &args.piljson)?;
+    write!(log_file, "const_pols:{}\n", &args.const_pols)?;
+    write!(log_file, "cm_pols:{}\n", &args.cm_pols)?;
+    write!(log_file, "proof:{}\n", &args.zkin)?;
     write!(log_file, "circom:{}\n", &args.circom_file)?;
 
-   
     let exec_result = stark_prove(
         &args.stark_struct,
         &args.piljson,
         //args.norm_stage,  //The current gevulot release version doesn't support bool in its "cmd_args",eg. {"name":"--norm_stage","value": ""}. The engineer gives a patch for the bug :
-                            //crates/cli/src/lib.rs  +138
-                            /*let step = WorkflowStep {
-                                program: (&(hex::decode(args.program)
-                                    .map_err(|err| format!("program decoding hash error:{err}"))?)[..])
-                                    .into(),
-                                args: args
-                                    .cmd_args
-                                    .into_iter()
-                                    .flat_map(<[String; 2]>::from)
-                                    .filter(|x| !x.is_empty())  //fix the bug which does not support bool in the "cmd_args" .
-                                    .collect(),
-                                inputs: input_data,
-                            }; */
+        //crates/cli/src/lib.rs  +138
+        /*let step = WorkflowStep {
+            program: (&(hex::decode(args.program)
+                .map_err(|err| format!("program decoding hash error:{err}"))?)[..])
+                .into(),
+            args: args
+                .cmd_args
+                .into_iter()
+                .flat_map(<[String; 2]>::from)
+                .filter(|x| !x.is_empty())  //fix the bug which does not support bool in the "cmd_args" .
+                .collect(),
+            inputs: input_data,
+        }; */
         true,
         args.skip_main,
         args.agg_stage,
@@ -95,7 +103,6 @@ fn run_task(task: Task) -> Result<TaskResult> {
         &args.prover_addr,
     );
 
-    
     match exec_result {
         Err(x) => {
             log::info!("The prover has error: {}", x);
@@ -110,6 +117,12 @@ fn run_task(task: Task) -> Result<TaskResult> {
     //std::fs::write("/workspace/proof.json", b"this is a proof a.")?;
 
     //return three files for Verifier
-    task.result(vec![], vec![String::from("/workspace/proof.json"),String::from("/workspace/stark_verfier.circom"),String::from("/workspace/test.log")])
-
+    task.result(
+        vec![],
+        vec![
+            String::from("/workspace/proof.json"),
+            String::from("/workspace/stark_verfier.circom"),
+            String::from("/workspace/test.log"),
+        ],
+    )
 }
